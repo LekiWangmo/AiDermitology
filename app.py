dcf59703421b6df90ff1aea3989d87d9db05df25
Kunzang Cheki <12220045.gcit@rub.edu.bt>
2024-05-29 20:50:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -131,3 +131,18 @@ def product():
 if __name__ == '__main__':
     app.run()
 
+@app.route('/index')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/scan')
+def scan():
+    return render_template('scan.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
